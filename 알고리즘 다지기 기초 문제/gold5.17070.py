bc129ade282125ff4fcf7c40e50d46616a01a8ad
whitemark <whtmdgus56@nate.com>
2022-12-10 01:03:16 +0900

    2022.12.10

--- 알고리즘 다지기 기초 문제/gold5.17070.py
+++ 알고리즘 다지기 기초 문제/gold5.17070.py
@@ -1,39 +1,28 @@
+# DFS - TIME LIMIT
+'''
 width = int(input())
 miro = []
 
 for i in range(width):
     miro.append(list(map(int,input().split())))
 
-answer = 0
-def dfs(x,y,state):
+def dfs(row,col,state):
     global answer
-    if x >= width-1 and state == 0:
-        return
-    if y >= width-1 and state == 2:
-        return
-    elif miro[x][y] == 1:
-        return
-    elif x == width-1 and y == width-1:
+    if row == width-1 and col == width-1:
         answer += 1
+        return
     
-    if state == 0:
-        if miro[x+1][y] == 0:
-            dfs(x+1,y,0)
-        if miro[x+1][y] == 0 and miro[x+1][y+1] == 0 and miro[x][y+1] == 0:
-            dfs(x+1,y+1,1)
-    elif state == 1:
-        if miro[x+1][y] == 0:
-            dfs(x+1,y,0)
-        if miro[x+1][y] == 0 and miro[x+1][y+1] == 0 and miro[x][y+1] == 0:
-            dfs(x+1,y+1,1)
-        if miro[x][y+1] == 0:
-            dfs(x,y+1,2)
-    elif state == 2:
-        if miro[x+1][y] == 0 and miro[x+1][y+1] == 0 and miro[x][y+1] == 0:
-            dfs(x+1,y+1,1)
-        if miro[x][y+1] == 0:
-            dfs(x,y+1,2)
-        
-dfs(1,0,0)
-
+    if state == 0 or state == 1:
+        if col < width-1 and miro[row][col+1] == 0:
+            dfs(row,col+1,0)
+    if state == 1 or state == 2:
+        if row < width-1 and miro[row+1][col] == 0:
+            dfs(row+1,col,2)
+    if state == 0 or state == 1 or state == 2:
+        if col < width-1 and row < width-1 and miro[row][col+1] == 0 and miro[row+1][col] == 0 and miro[row+1][col+1] == 0:
+            dfs(row+1,col+1,1)
+    
+answer = 0
+dfs(0,1,0)
 print(answer)
+'''
