7d2173d845409582f18d15144428f3f5461319bb
whitemark <whtmdgus56@nate.com>
2022-12-11 23:39:12 +0900

    2022.12.11

--- 알고리즘 다지기 기초 문제/gold5.17070.py
+++ 알고리즘 다지기 기초 문제/gold5.17070.py
@@ -26,3 +26,25 @@ answer = 0
 dfs(0,1,0)
 print(answer)
 '''
+
+# DP
+
+width = int(input())
+graph = [list(map(int, input().split())) for _ in range(width)]
+dp = [[[0] * width for _ in range(width)] for _ in range(3)]
+dp[0][0][1] = 1
+
+for i in range(0, width):
+    for j in range(1, width):
+        
+        if i == 0 and j == 1:
+            continue
+        
+        if graph[i][j] == 0 and graph[i - 1][j] == 0 and graph[i][j - 1] == 0:
+            dp[2][i][j] = sum(dp[k][i - 1][j - 1] for k in range(3))
+        
+        if graph[i][j] == 0:
+            dp[0][i][j] = dp[0][i][j - 1] + dp[2][i][j - 1]
+            dp[1][i][j] = dp[1][i - 1][j] + dp[2][i - 1][j]
+        
+print(sum(dp[k][-1][-1] for k in range(3)))
